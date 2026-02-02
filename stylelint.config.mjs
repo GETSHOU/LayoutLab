06d4fe125fcba22aba8e71dfc26a5b7be00dac27
Getshou <bsf.work.mailbox@mail.ru>
2026-02-02 18:30:10 +0300

    stylelint new rules added

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('stylelint').Config} */
+
 export default {
 	extends: ['stylelint-config-standard-scss'],
-	rules: {},
+	rules: {
+		'block-no-empty': true,
+		'no-empty-source': false,
+		'comment-empty-line-before': 'never',
+	},
 };
